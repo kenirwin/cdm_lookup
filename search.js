12f0e0142df0b5fe66770d53b1c59e990e66270c
Ken Irwin <irwinkr@miamioh.edu>
2021-03-26 19:52:38 -0400

    added lookups for itemMetadata and itemUrl

--- search.js
+++ search.js
@@ -1,16 +1,21 @@
+const baseUrl =
+  'https://cdm17240.contentdm.oclc.org/digital/bl/dmwebservices/index.php?q=';
+const format = 'json';
+const collection = 'postcards';
+
 function GetSearchUrl(searchTerms) {
-  const collection = 'postcards';
   const sortBy = 'no_sort';
   const maxRecs = '';
   const start = 0;
-  const format = 'json';
+
   const searchField = 'CISOSEARCHALL';
   searchTerms = searchTerms.replace(' ', '+');
   const searchMode = 'all';
   searchOperator = 'and';
   let searchString = `${searchField}^${searchTerms}^${searchMode}^${searchOperator}`;
   queryUrl =
-    'https://cdm17240.contentdm.oclc.org/digital/bl/dmwebservices/index.php?q=dmQuery/' +
+    baseUrl +
+    'dmQuery/' +
     collection +
     '/' +
     searchString +
@@ -23,4 +28,14 @@ function GetSearchUrl(searchTerms) {
     '/1/0/0/0/0/0/' +
     format;
   return queryUrl;
+}
+
+function getItemMetadataUrl(pointer) {
+  getMetadataUrl =
+    baseUrl + 'dmGetItemInfo/' + collection + '/' + pointer + '/' + format;
+  return getMetadataUrl;
+}
+
+function getItemUrl(find) {
+  return baseUrl + 'dmGetItemUrl/' + collection + '/' + find + '/' + format;
 }
